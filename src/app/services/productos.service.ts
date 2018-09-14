07f56ba736ce72a963bb16040bdfad9813b9315a
Gonzalo AP <gonzaloapp@gmail.com>
2018-09-14 09:51:17 +0200

    fin seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,19 +9,51 @@ export class ProductosService {
 
   cargando = true;
   productos: ProductoInterface[] = [];
+  productosFiltrado: ProductoInterface[] = [];
 
   constructor( public http: HttpClient) {
     this.cargarProductos();
    }
 
   private cargarProductos() {
-    this.http.get('https://angular-html-1c563.firebaseio.com/productos_idx.json').subscribe( (resp: ProductoInterface[]) => {
+    return new Promise( ( resolve, reject ) => {
+      this.http.get('https://angular-html-1c563.firebaseio.com/productos_idx.json').subscribe( (resp: ProductoInterface[]) => {
       console.log(resp);
       this.productos = resp;
 
       setTimeout(() => {
         this.cargando = false;
       }, 2000);
+      resolve();
+    });
+    });
+  }
+
+  getProducto(id: string) {
+    return this.http.get(`https://angular-html-1c563.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto(termino: string) {
+    if (this.productos.length === 0) {
+      this.cargarProductos().then( () => {
+        this.filtrarProducto(termino);
+      });
+    } else {
+      this.filtrarProducto(termino);
+    }
+    console.log(this.productosFiltrado);
+  }
+
+  filtrarProducto(termino: string) {
+    this.productosFiltrado = [];
+    this.productos.forEach( prod => {
+
+      const titulo: string = prod.titulo.toLowerCase();
+      const categoria: string = prod.categoria.toLowerCase();
+
+      if (categoria.indexOf(termino) >= 0 || titulo.indexOf(termino) >= 0) {
+        this.productosFiltrado.push(prod);
+      }
     });
   }
 }
